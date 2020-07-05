7d60555a9d335661521b333c470c7395fe35427d
George Lam <2940746+GeorgeLam@users.noreply.github.com>
2020-07-05 22:23:22 +0800

    Added draft language data, Started writing new recommendation algorithm

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function showTab(n){
 document.querySelectorAll("#button-1").forEach(item => 
     item.addEventListener("click", (e) => {
         e.preventDefault();
-        if (currentTab == 1) {recc();}
+        if (currentTab == 1) {recc2();}
         showTab(++currentTab)
         }
     )
@@ -37,3 +37,78 @@ document.querySelector("#retry").addEventListener("click", (e) => {
     e.preventDefault();
     showTab(currentTab = 0);
 })
+
+
+//// Language data
+languages = [
+    {
+        "name": "French",
+        "en-difficulty": 1,
+        "business": 4,
+        "culture": 5
+    }, {
+        "name": "Italian",
+        "en-difficulty": 1,
+        "business": 1,
+        "culture": 4
+    }, {
+        "name": "Spanish",
+        "en-difficulty": 1,
+        "business": 3,
+        "culture": 3
+    }, {
+        "name": "Swedish",
+        "en-difficulty": 1,
+        "business": 1,
+        "culture": 2
+    }, {
+        "name": "German",
+        "en-difficulty": 2,
+        "business": 5,
+        "culture": 3
+    },{
+        "name": "Swahili",
+        "en-difficulty": 3,
+        "business": 2,
+        "culture": 2
+    },{
+        "name": "Russian",
+        "en-difficulty": 4,
+        "business": 3,
+        "culture": 3
+    },{
+        "name": "Hindi",
+        "en-difficulty": 4,
+        "business": 4,
+        "culture": 3
+    },{
+        "name": "Hebrew",
+        "en-difficulty": 4,
+        "business": 2,
+        "culture": 1
+    },{
+        "name": "Arabic",
+        "en-difficulty": 5,
+        "business": 4,
+        "culture": 2
+    },{
+        "name": "Chinese",
+        "en-difficulty": 5,
+        "business": 5,
+        "culture": 3
+    },{
+        "name": "Japanese",
+        "en-difficulty": 5,
+        "business": 3,
+        "culture": 4
+    }
+]
+
+function recc2(){
+    let suitability = [];
+    languages.forEach(item => {
+        if(parseInt(difficulty.value) >= item["en-difficulty"]){
+            suitability.push(item)}
+    })
+    console.log(suitability)
+}
